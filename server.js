8d0abc0f3b837a9ec5d1bd2fdcd6f6e4d8faef98
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-01-19 21:18:24 +0545

    CRUD

--- server.js
+++ server.js
@@ -6,6 +6,39 @@ const swaggerDocument = YAML.load("./swagger.yaml");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+let items = [{ id: 1, name: "Item 1" }];
+
+//CRUD
+app.get("/items", (req, res) => {
+  res.json(items);
+});
+
+app.post("/items", (req, res) => {
+  const item = {
+    id: items.length + 1,
+    name: req.body.name,
+  };
+
+  items.push(item);
+  res.status(201).send(item);
+});
+
+app.put("/items/:id", (req, res) => {
+  const item = items.find((item) => item.id === parseInt(req.params.id));
+  if (!item) return res.status(404).send("Item not found");
+
+  item.name = req.body.name;
+  res.send(item);
+});
+
+app.delete("/items/:id", (req, res) => {
+  items = items.filter((item) => item.id !== parseInt(req.params.id));
+  res.status(204).send();
+});
+
+//Swagger UI setup
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
