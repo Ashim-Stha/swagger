1807c783bce082a71a9f7f6b8680160ca64a964b
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-01-19 21:08:07 +0545

    refactored server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const app = express();
+const PORT = 3000;
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello World!");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("object");
 });
